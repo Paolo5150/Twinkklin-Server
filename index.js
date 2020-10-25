51b74ae2409c245e0cae8f7aefcdeeaf169b20d0
Paolo_X86 <p.ferri1986@gmail.com>
2020-10-25 10:59:05 +0800

    Testing db connection

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Works!")
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "us-cdbr-east-02.cleardb.com",
+  user: "b2b800ec04abfa",
+  password: "9ccdfa55",
+  database: "heroku_46d8c893b61bb88"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  con.query("SELECT * FROM jewels", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
